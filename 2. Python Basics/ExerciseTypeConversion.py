d5b2733d7bd271d29d1c125222c7c1b45cc2aaa4
Nitesh Kumar <nkniteshkumarsingh@gmail.com>
2023-04-22 21:46:46 +0530

    Adding Int Str codes

---
+++ 2. Python Basics/ExerciseTypeConversion.py
@@ -0,0 +1,6 @@
+# Exercise Type Conversion
+# Write a program which tells your age
+
+birth_year = input('What year were you born?\n')
+age = 2023 - int(birth_year)
+print(f'Your age is {age} years.')
